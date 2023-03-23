72c78e3afafadb3652e9f9ed1e56aa70b748aa38
Areg <aregam96>
2023-03-23 14:33:11 +0400

    newedition1

--- usersync.sh
+++ usersync.sh
@@ -13,7 +13,7 @@ git clone "$REPO_URL" --quiet
 cd "$(basename "$REPO_URL" .git)"
 
 # Read the CSV file line by line and clone the users
-while IFS=',' read -r ID Name Email Slack GitHub Team Jira
+while IFS=',' read -r Name Email GitHub
 do
     echo "Cloning $username"
     git clone "https://$Name:$Email:$GitHub@github.com/$username/your_repository.git" --quiet
