d4497de5703cad5d51036380023cb14f61a8b89c
Areg <aregam96>
2023-03-28 12:23:32 +0400

    groupchange

--- usersync.sh
+++ usersync.sh
@@ -4,10 +4,65 @@
 GITHUB_TOKEN=ithub_pat_11A6BAHBY0v4FgBb16gAde_KiWYD6ABr5LJcaJaqDTkgzX1B7y65N9inPqyASeSNEbNHVHR4LDJGkMfaM6
 REPO_URL=https://github.com/aregam96/usersync.git
 
-#file name and path for the usersync.csv file
+#File name and path for the csv file
 FILENAME="usersync.csv"
 FILEPATH="./$FILENAME"
 
-# Clone the repository using the GitHub API token
+#Clone the repository using the GitHub API token
 git clone "$REPO_URL" --quiet
 cd "$(basename "$REPO_URL" .git)"
+
+#parse the file version 3
+while IFS="," read -r rec_column1 rec_column2 rec_column3
+do
+  echo "Name-$rec_column1"
+  echo "Email: $rec_column2"
+  echo "Team: $rec_column3"
+  echo ""
+done < <(tail -n +2 $FILENAME)
+
+# Set the name of the CSV file
+csv_file=$FILENAME
+
+# Set the name of the Azure AD tenant
+#tenant_name="AregTestingEnv"
+
+# Set the name of the Azure AD app
+app_name="aregtest"
+
+# Authenticate to Azure using the Azure CLI
+az login
+
+# Loop through each row in the CSV file
+while IFS=',' read -r name email team; do
+
+# Check if the user already exists in Azure AD
+  if az ad user show --upn-or-object-id "$email" &>/dev/null; then
+    echo "User $name ($email) already exists in Azure AD."
+  else
+    # Create the user in Azure AD
+    password=$(openssl rand -base64 12)
+    az ad user create \
+      --display-name "$name" \
+      --user-principal-name "$email" \
+      --password "$password" \
+      --output none
+    echo "Created user $name ($email) with password $password." 
+  fi
+
+  # Check if the group already exists in Azure AD
+  if ! az ad group show --group "$team" &>/dev/null; then
+    # Create the group in Azure AD
+    az ad group create \
+      --display-name "$team" \
+      --mail-nickname "$team" \
+      --output none
+    echo "Created group $team."
+  fi
+
+  # Add the user to the specified group
+  az ad group member check --group "$team" --member-id "$email" &>/dev/null || \
+    az ad group member add --group "$team" --member-id "$email" &>/dev/null
+  echo "Added user $name ($email) to group $team."
+
+done < "$csv_file"
