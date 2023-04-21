c91363d18a95e031ab3344d2d8d1542d0ce6845b
Areg <aregam96>
2023-04-21 14:47:51 +0400

    commentedeverything

--- usersync.sh
+++ usersync.sh
@@ -30,45 +30,45 @@
 az login --service-principal --username "$1" -p="$2" --tenant "$3"
 # az login --service-principal --username 8a839d12-a3c5-4dc7-8561-a7c1a80bafb4 -p=lCD8Q~SENxUhx5lFWtQl1Rz232mEo__uHUXEJa9Y --tenant 7c2be92f-7fb6-4331-9453-11501f1f57e3
 
-# Loop through each row in the CSV file for user check/creation
- while read line || [ -n "$line" ]
- do 
-    # Extract the user principal name and team from the line
-    name=$(echo "$line" | awk -F "," '{ print $1}')
-    email=$(echo "$line" | awk -F "," '{ print $2}')
-    team=$(echo "$line" | awk -F "," '{ print $3}')
-    # Check if the user already exists in Azure AD
-    if az ad user show --id "$email" &>/dev/null; then 
-        log "User $name ($email) already exists in Azure AD." "INFO"
-    else
-        #Create the user in Azure AD
-        password=$(openssl rand -base64 12)
-        az ad user create \
-            --display-name "$name" \
-            --user-principal-name "$email" \
-            --password "$password" \
-            --output none
-        log "Created user $name ($email) with password $password." "INFO"
-    fi 
+# # Loop through each row in the CSV file for user check/creation
+#  while read line || [ -n "$line" ]
+#  do 
+#     # Extract the user principal name and team from the line
+#     name=$(echo "$line" | awk -F "," '{ print $1}')
+#     email=$(echo "$line" | awk -F "," '{ print $2}')
+#     team=$(echo "$line" | awk -F "," '{ print $3}')
+#     # Check if the user already exists in Azure AD
+#     if az ad user show --id "$email" &>/dev/null; then 
+#         log "User $name ($email) already exists in Azure AD." "INFO"
+#     else
+#         #Create the user in Azure AD
+#         password=$(openssl rand -base64 12)
+#         az ad user create \
+#             --display-name "$name" \
+#             --user-principal-name "$email" \
+#             --password "$password" \
+#             --output none
+#         log "Created user $name ($email) with password $password." "INFO"
+#     fi 
 
- done < <(tail -n +2 "$FILENAME")
+#  done < <(tail -n +2 "$FILENAME")
 
-# Loop through each line in the CSV file for group check/add
- while read line || [ -n "$line" ]
- do 
-    # Extract the user principal name and team from the line
-    email=$(echo "$line" | awk -F "," '{ print $2}')
-    team=$(echo "$line" | awk -F "," '{ print $3}')
+# # Loop through each line in the CSV file for group check/add
+#  while read line || [ -n "$line" ]
+#  do 
+#     # Extract the user principal name and team from the line
+#     email=$(echo "$line" | awk -F "," '{ print $2}')
+#     team=$(echo "$line" | awk -F "," '{ print $3}')
 
-    # Get the object ID of the user using the Azure CLI
-    user_id=$(az ad user show --id "$email" --query id --output tsv)
+#     # Get the object ID of the user using the Azure CLI
+#     user_id=$(az ad user show --id "$email" --query id --output tsv)
 
-    # Check if the user is a member of the group
-    # msg=az ad group member add --group "$team" --member-id "$user_id" 2>&1
-    if az ad group member add --group "$team" --member-id "$user_id" >/dev/null 2>&1; then
-        log "Added user $email to group $team" "INFO"
-    else
-        error_code=$?
-        log "Error $error_code:" "ERROR"
-    fi
- done < <(tail -n +2 "$FILENAME")
+#     # Check if the user is a member of the group
+#     # msg=az ad group member add --group "$team" --member-id "$user_id" 2>&1
+#     if az ad group member add --group "$team" --member-id "$user_id" >/dev/null 2>&1; then
+#         log "Added user $email to group $team" "INFO"
+#     else
+#         error_code=$?
+#         log "Error $error_code:" "ERROR"
+#     fi
+#  done < <(tail -n +2 "$FILENAME")
