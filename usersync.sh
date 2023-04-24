85c1a6df8b8dc25a9ae6f1b0e00ca1b00031e7cd
amazmanyan <amazmanyan@hubspot.com>
2023-04-24 21:32:06 +0400

    some commit

--- usersync.sh
+++ usersync.sh
@@ -4,6 +4,8 @@ echo "Username: $1"
 echo "Password: $2"
 echo "Tenant: $3"
 
+aergertshsetr
+
 # GitHub API token and repository URL
  #     GITHUB_TOKEN=ithub_pat_11A6BAHBY0v4FgBb16gAde_KiWYD6ABr5LJcaJaqDTkgzX1B7y65N9inPqyASeSNEbNHVHR4LDJGkMfaM6
  #     REPO_URL=https://github.com/aregam96/usersync.git
